d5fd74be966df6db80f87235f5dc3ac5d0e0de85
Eurisko83200 <t.oustrain@gmail.com>
2021-02-12 06:25:09 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,481 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-salle--manger",
+      "name": "Salle à manger",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.0006129100000027421,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5715920460199477,
+          "pitch": 0.3483863316273581,
+          "rotation": 1.5707963267948966,
+          "target": "9-salon"
+        },
+        {
+          "yaw": 2.1131984800321844,
+          "pitch": -0.09013642113742648,
+          "rotation": 5.497787143782138,
+          "target": "6-cage-descalier"
+        },
+        {
+          "yaw": 1.3569729391670382,
+          "pitch": 0.018233897377047725,
+          "rotation": 1.5707963267948966,
+          "target": "3-suite-parentale"
+        },
+        {
+          "yaw": 0.8595110830329595,
+          "pitch": -0.04219805572047619,
+          "rotation": 3.9269908169872414,
+          "target": "1-cuisine"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cuisine",
+      "name": "Cuisine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3150288181305747,
+          "pitch": -0.016551128750979416,
+          "rotation": 1.5707963267948966,
+          "target": "3-suite-parentale"
+        },
+        {
+          "yaw": 2.4053290502375813,
+          "pitch": -0.04119390683839441,
+          "rotation": 5.497787143782138,
+          "target": "6-cage-descalier"
+        },
+        {
+          "yaw": -2.6892678418433142,
+          "pitch": -0.03830796052472252,
+          "rotation": 2.356194490192345,
+          "target": "0-salle--manger"
+        },
+        {
+          "yaw": 3.141225704365562,
+          "pitch": 0.21828110452658223,
+          "rotation": 13.351768777756625,
+          "target": "9-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-salle-deau-suite-parentale",
+      "name": "Salle d'eau suite parentale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.25563016571449815,
+          "pitch": -0.10224161348127225,
+          "rotation": 1.5707963267948966,
+          "target": "3-suite-parentale"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-suite-parentale",
+      "name": "Suite parentale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5200605811530359,
+          "pitch": 0.06224946770950801,
+          "rotation": 4.71238898038469,
+          "target": "4-suite-parentale---dressing"
+        },
+        {
+          "yaw": -1.4657489008985038,
+          "pitch": 0.04289785680618685,
+          "rotation": 4.71238898038469,
+          "target": "2-salle-deau-suite-parentale"
+        },
+        {
+          "yaw": -0.213259211282244,
+          "pitch": -0.013120630036311809,
+          "rotation": 4.71238898038469,
+          "target": "1-cuisine"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-suite-parentale---dressing",
+      "name": "Suite parentale - dressing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8884810848665552,
+          "pitch": -0.039925990595399696,
+          "rotation": 0,
+          "target": "3-suite-parentale"
+        },
+        {
+          "yaw": -0.5405478787717044,
+          "pitch": -0.04346745861121093,
+          "rotation": 4.71238898038469,
+          "target": "2-salle-deau-suite-parentale"
+        },
+        {
+          "yaw": 0.15063978667522626,
+          "pitch": -0.05394156997349775,
+          "rotation": 1.5707963267948966,
+          "target": "1-cuisine"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-chambre-3",
+      "name": "Chambre 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0366579298507794,
+          "pitch": 0.4796962657562016,
+          "rotation": 0,
+          "target": "6-cage-descalier"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-cage-descalier",
+      "name": "Cage d'escalier",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04905469076594393,
+          "pitch": -0.05303506887245568,
+          "rotation": 0,
+          "target": "7-salle-de-bains-tage"
+        },
+        {
+          "yaw": 1.5053738683282525,
+          "pitch": 0.06232966524931882,
+          "rotation": 0,
+          "target": "8-chambre-2"
+        },
+        {
+          "yaw": -1.5795386580909057,
+          "pitch": 0.4665065671262614,
+          "rotation": 0,
+          "target": "5-chambre-3"
+        },
+        {
+          "yaw": 2.3087650617745448,
+          "pitch": 0.1118949525932873,
+          "rotation": 2.356194490192345,
+          "target": "0-salle--manger"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-salle-de-bains-tage",
+      "name": "Salle de bains étage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.505546552824983,
+          "pitch": -0.07288574858406882,
+          "rotation": 4.71238898038469,
+          "target": "6-cage-descalier"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-chambre-2",
+      "name": "Chambre 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.604712097750788,
+          "pitch": 0.4207740430560385,
+          "rotation": 0,
+          "target": "6-cage-descalier"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2017079139836344,
+          "pitch": 0.16642119466050964,
+          "rotation": 4.71238898038469,
+          "target": "0-salle--manger"
+        },
+        {
+          "yaw": 1.4245208730405103,
+          "pitch": 0.08332305786938576,
+          "rotation": 0,
+          "target": "1-cuisine"
+        },
+        {
+          "yaw": 1.7179352533235406,
+          "pitch": 0.11783149868466936,
+          "rotation": 5.497787143782138,
+          "target": "6-cage-descalier"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
